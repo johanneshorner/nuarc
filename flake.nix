26469df1053658c59374ae340db297e4989471c7
Johannes Horner <lzy.info@pm.me>
2025-07-06 23:54:50 +0200

    package for nix

---
+++ flake.nix
@@ -0,0 +1,62 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+  };
+
+  outputs =
+    {
+      self,
+      nixpkgs,
+    }:
+    let
+      nuarc-package =
+        {
+          lib,
+          stdenv,
+          nushell,
+          makeWrapper,
+        }:
+
+        stdenv.mkDerivation {
+          pname = "nuarc";
+          version = self.shortRev or self.dirtyShortRev or "unknown";
+
+          src = ./.;
+
+          installPhase = ''
+            mkdir -p $out/bin
+            cp ./nuarc $out/bin/
+          '';
+
+          meta = {
+            homepage = "https://github.com/johanneshorner/nuarc";
+            description = "A nushell script to control aruba switches";
+            mainProgram = "nuarc";
+          };
+        };
+
+      inherit (nixpkgs) lib;
+      # Support all Linux systems that the nixpkgs flake exposes
+      systems = lib.intersectLists lib.systems.flakeExposed lib.platforms.linux;
+
+      forAllSystems = lib.genAttrs systems;
+      nixpkgsFor = forAllSystems (system: nixpkgs.legacyPackages.${system});
+    in
+    {
+      packages = forAllSystems (
+        system:
+        let
+          rustic = nixpkgsFor.${system}.callPackage nuarc-package { };
+        in
+        {
+          inherit rustic;
+          default = rustic;
+          withMount = rustic.override { withMount = true; };
+        }
+      );
+
+      overlays.default = final: _: {
+        rustic = final.callPackage nuarc-package { };
+      };
+    };
+}
